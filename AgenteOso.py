e25bd629891a31a88d14632edf2f44b4b3a9e4e5
laura Meneses <lmeneses58@gmail.com>
2020-10-04 22:19:39 -0400

    validaciones

--- AgenteOso.py
+++ AgenteOso.py
@@ -15,31 +15,43 @@ class AgenteOso(AgenteJugador):
 
     def getResultado(self, estado, m):
         x,y,ficha = m
+        m1 = x,y
         if m not in estado.movidas:
-            return ElEstado(jugador=('A' if estado.jugador == 'B' else 'B'),
-                            get_utilidad=self.computa_utilidad(estado.tablero, m, estado.jugador),
+            utilidad = self.computa_utilidad(estado.tablero, m, estado.jugador)
+            if utilidad != 0:
+                return ElEstado(jugador=('A' if estado.jugador == 'B' else 'B'), cantidad_osos = estado.cantidad_osos + 1,
+                            get_utilidad=utilidad,
                             tablero=estado.tablero, movidas=estado.movidas)
 
         tablero = estado.tablero.copy()
-        #tablero[m] = estado.jugador
-        tablero[m] = ficha
+        tablero[m1] = ficha
         movidas = list(estado.movidas)
-        movidas.remove(m)
-        return ElEstado(jugador=('A' if estado.jugador == 'B' else 'B'),
-                        get_utilidad=self.computa_utilidad(tablero, m, estado.jugador),
-                        tablero=tablero, movidas=movidas)
+        if (m in movidas):
+            movidas.remove(m)
+            utilidad2 = self.computa_utilidad(tablero, m, estado.jugador)
+            if utilidad2 != 0:
+                return ElEstado(jugador=('A' if estado.jugador == 'B' else 'B'), cantidad_osos = estado.cantidad_osos + 1,
+                            get_utilidad=utilidad2,
+                            tablero=tablero, movidas=movidas)
+            else:
+                return ElEstado(jugador=('A' if estado.jugador == 'B' else 'B'), cantidad_osos = estado.cantidad_osos,
+                            get_utilidad=utilidad2,
+                            tablero=tablero, movidas=movidas)
+        else:
+            print("jugada invalida")
 
     def get_utilidad(self, estado, jugador):
         return estado.get_utilidad if jugador == 'B' else -estado.get_utilidad
 
     def testTerminal(self, estado):
-        return estado.get_utilidad != 0 or len(estado.movidas) == 0
+        #print("testTerminal "+ str(len(estado.movidas)))
+        return (len(estado.movidas) - (self.h*self.v) + 1) == 0 
 
     def mostrar(self, estado):
         tablero = estado.tablero
         for x in range(1, self.h + 1):
             for y in range(1, self.v + 1):
-                print(tablero.get((x, y), '.')+" ", end="")
+                print(tablero.get((x, y), '.') + " ", end="")
             print()
 
     def computa_utilidad(self, tablero, m, jugador):
@@ -54,22 +66,20 @@ class AgenteOso(AgenteJugador):
     def en_raya(self, tablero, m, jugador, delta_x_y):
         (delta_x, delta_y) = delta_x_y
         x, y, ficha = m
-        resultado = ['A','B','A']
+        resultado = ['O','S','O']
         c = 0
         n = 0
-        while c<3 and c>=0:
-            if tablero.get((x, y)) == resultado[c]:
-                #print(tablero.get((x, y)) + "  " + resultado[c] + "  " + str(c))
+        while 0 <= c < 3:
+            if tablero.get((x,y)) == resultado[c]:
                 n += 1
                 c += 1
                 x, y = x + delta_x, y + delta_y
-                print("contador " + str(c))
             else:
                 c = 3
         x, y, ficha = m
         c = 0
-        while c<3 and c>=0:
-            if tablero.get((x, y)) == resultado[c]:
+        while 0 <= c < 3:
+            if tablero.get((x,y)) == resultado[c]:
                 n += 1
                 c += 1
                 x, y = x - delta_x, y - delta_y
--- Tablero.py
+++ Tablero.py
@@ -14,7 +14,7 @@ class Tablero(Entorno):
         movidasConO = [(x, y,'O') for x in range(1, h + 1) for y in range(1, v + 1)]
         movidas = movidasConS + movidasConO
 
-        self.juegoActual = ElEstado(jugador='A', get_utilidad=0, tablero={}, movidas=movidas)
+        self.juegoActual = ElEstado(jugador='A',cantidad_osos = 0, get_utilidad=0, tablero={}, movidas=movidas)
         self.ancho = 600
         self.alto = 600
         pg.init()
@@ -67,11 +67,11 @@ class Tablero(Entorno):
     def marcar(self, row, col, ficha, jugador):
         x_img = pg.image.load("img/X_modified.png")
         y_img = pg.image.load("img/o_modified.png")
-        x_img = pg.transform.scale(x_img, (80, 80))
-        o_img = pg.transform.scale(y_img, (80, 80))
+        x_img = pg.transform.scale(x_img, (60, 60))
+        o_img = pg.transform.scale(y_img, (60, 60))
         posx, posy = 0, 0
         if row == 1:
-            posx = 30
+            posx = 0
         if row == 2:
             posx = self.ancho / 8 
         if row == 3:
@@ -87,7 +87,7 @@ class Tablero(Entorno):
         if row == 8:
             posx = self.ancho / 8 * 7 
         if col == 1:
-            posy = 30
+            posy = 0
         if col == 2:
             posy = self.alto / 8 
         if col == 3:
@@ -102,29 +102,11 @@ class Tablero(Entorno):
             posy = self.alto / 8 * 6 
         if col == 8:
             posy = self.alto / 8 * 7 
-        if jugador == 'B':
+        if ficha == 'S':
             self.ventana.blit(x_img, (posy, posx))
         else:
             self.ventana.blit(o_img, (posy, posx))
-        pg.display.update()
-
-    def seleccionar_opcion(self):
-        mouse = pg.mouse.get_pos()
-        ficha = 'O'
-        op_x_selected = pg.image.load('img/X_modified_selected.png')
-        op_y_selected = pg.image.load('img/o_modified_selected.png')
-        op_x_selected = pg.transform.scale(op_x_selected, (50, 50))
-        op_y_selected = pg.transform.scale(op_y_selected, (60, 60))
-        if event.type is MOUSEBUTTONDOWN and (620 + 50 > x > 620 and 200 + 50 > y > 200):          
-            self.ventana.blit(op_x_selected, (620, 200))
-            ficha = 'X'
-        if event.type is MOUSEBUTTONDOWN and (620 + 60 > x > 620 and 400 + 60 > y > 400):       
-            self.ventana.blit(op_y_selected, (620, 400))
-            ficha = 'O'
-        
-        print(ficha)
-        return ficha
-
+        #pg.display.update()
 
     def accion_humano(self, age, ficha):
         x, y = pg.mouse.get_pos()
@@ -200,25 +182,22 @@ class Tablero(Entorno):
                     pg.quit()
                     sys.exit()
                 if self.agentes[actual].__class__.__name__ == "HumanoOso":
-                    #controla ficha elegida
-                    # ficha = self.seleccionar_opcion()
-                    if event.type is MOUSEBUTTONDOWN and (620 + 50 > x > 620 and 200 + 50 > y > 200):
-                        self.ventana.blit(op_x_selected, (620, 200))
-                        self.ventana.blit(op_y_img, (620, 400))
-                        ficha = 'X'
-            
+
                     if event.type is MOUSEBUTTONDOWN and (620 + 60 > x > 620 and 400 + 60 > y > 400):         
-                        self.ventana.blit(op_y_selected, (620, 400))
                         self.ventana.blit(op_x_img, (620, 200))
+                        self.ventana.blit(op_y_selected, (620, 400))
                         ficha = 'O'
 
-                    print (ficha + " opcion seleccionada")
+                    if event.type is MOUSEBUTTONDOWN and (620 + 50 > x > 620 and 200 + 50 > y > 200):
+                        self.ventana.blit(op_y_img, (620, 400))
+                        self.ventana.blit(op_x_selected, (620, 200))
+                        ficha = 'S'
+            
                     if event.type is MOUSEBUTTONDOWN and (0 <= x < self.ancho and 0 <= y < self.alto):
-                        print("juega humano")
                         self.agentes[actual].estado = self.juegoActual
                         if self.agentes[actual].estado.movidas:
-                            print(ficha + "dentro de humano")
                             self.accion_humano(self.agentes[actual],ficha)
+                            print (self.agentes[actual].estado.cantidad_osos)
                         if self.agentes[actual].testTerminal(self.agentes[actual].getResultado(self.juegoActual, self.agentes[actual].acciones)):
                             self.agentes[actual].vive = False
                         self.ejecutar(self.agentes[actual])
@@ -227,23 +206,22 @@ class Tablero(Entorno):
                     self.percibir(self.agentes[actual])
                     self.ejecutar(self.agentes[actual])
                     actual = 0
-                    print("juega maquina")
             tablero = self.juegoActual.tablero
-            for x, y,ficha in tablero.keys():
+            for x, y in tablero.keys():
                 self.marcar(x, y,ficha, tablero.get((x, y)))
             if self.finalizado():
-                a = self.juegoActual.get_utilidad
-                if a != 0:
-                    if a > 0:
+                print("osos " + self.agentes[actual].estado.cantidad_osos)
+                if self.agentes[0].estado.cantidad_osos > self.agentes[1].estado.cantidad_osos :
                         self.ventana.blit(victoriaXImg, (400, 250))
-                        print("Victoria X ")
-                    else:
-                        self.ventana.blit(victoriaOImg, (400, 250))
-                        print("Victoria O ")
+                        print("Victoria " + self.agentes[0].estado.jugador)
                 else:
+                        self.ventana.blit(victoriaOImg, (400, 250))
+                        print("Victoria " + self.agentes[1].estado.jugador)
+                if self.agentes[0].estado.cantidad_osos == self.agentes[1].estado.cantidad_osos :
                     self.ventana.blit(victoriaEmpate, (400, 250))
-                    print("Empate")
+                    print("Empate" + self.juegoActual.jugador)
                 pg.quit()
                 sys.exit()
             pg.display.update()
             tiempo.tick(fps)
+
